54996062679e5d5b4dd6442f0557070bdba9ca95
Marcelo Arias <hola@marceloarias.com>
2022-05-29 21:35:17 -0500

    Clase 8 - Observables: fromEvent

--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
 /**
- * Creación de un Observable
- * Un Observable es un elemento que en RxJS representa el flujo de información.
- * Este flujo de información puede ser completado (línea 13 y 21), y también puede fallar (descomentar línea 12).
+ * Observables: fromEvent
+ * El método fromEvent nos permite generar observables que capturan eventos del DOM.
+ * https://rxjs.dev/api/index/function/fromEvent
  */
-import { Observable } from "rxjs";
+import { fromEvent } from "rxjs";
 
-const observableAlfa$ = new Observable((subscriber) => {
-  subscriber.next(1);
-  subscriber.next(2);
-  subscriber.next(3);
-  // a = b;
-  subscriber.complete();
-  subscriber.next(4);
-});
+const onKeyDown$ = fromEvent(document, "keydown");
 
-const observador = {
-  next: (value) => {
-    console.log(value); // Podemos transmitir los valores emitidos.
-  },
-  complete: () => {
-    console.log("Observable completado"); // Podemos informar que el Observable dejará de emitir valores.
-  },
-  error: (error) => {
-    console.log("Error recibido: "); // Podemos manejar errores provinientes del Observable.
-    console.error(error);
+const observadorMouse = {
+  next: (event) => {
+    console.log(event.key);
   },
 };
 
-// El observador se suscribe al Observable de esta forma:
-observableAlfa$.subscribe(observador);
+onKeyDown$.subscribe(observadorMouse);
