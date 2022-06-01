5513a833f24a6d627fab0798d4e3dc82a5604e94
Marcelo Arias <hola@marceloarias.com>
2022-06-01 12:38:12 -0500

    Clase 30 - AJAX en RxJS

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 /**
- * Manejo de errores en RxJS
- * https://rxjs.dev/api/operators/catchError
- * https://rxjs.dev/api/operators/retry
+ * AJAX en RxJS
+ * https://rxjs.dev/api/ajax/ajax
  */
-import { of, catchError, map, retry } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { of, map, catchError } from "rxjs";
 
-const letters$ = of("A", "B", "C", "D", "E").pipe(
-  map((letter) => {
-    if (letter === "D") {
-      x = 4;
-    }
-    return letter;
+// const ditto$ = ajax("https://pokeapi.co/api/v2/pokemon/itto").pipe(
+//   map((data) => console.log(data.response)),
+//   catchError((error) => {
+//     console.log("Error: ", error.message);
+//     return of(error);
+//   })
+// );
+
+// ditto$.subscribe(console.log);
+
+// https://pokeapi.co/api/v2/pokemon/ditto
+
+const postRequest$ = ajax({
+  url: "https://httpbin.org/delay/5",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: {
+    message: "¿Dónde está Ditto?",
+  },
+}).pipe(
+  map((response) => {
+    console.log(response);
+    return response;
   }),
-  retry(2),
-  catchError((error) => of(error.message))
+  catchError((error) => {
+    console.log("Error: ", error.message);
+    return of(error);
+  })
 );
 
-letters$.subscribe(console.log);
+postRequest$.subscribe(console.log);
