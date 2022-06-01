16cfc150adf4fac4e4b13f66ae7683a2988e1601
Marcelo Arias <hola@marceloarias.com>
2022-06-01 00:48:12 -0500

    Clase 25 - Operadores: pluck

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 /**
- * Operadores: takeUntil
- * https://rxjs.dev/api/operators/takeUntil
+ * Operadores: pluck
+ * https://rxjs.dev/api/operators/pluck
  */
-
 import { fromEvent } from "rxjs";
-import { takeUntil } from "rxjs/operators";
+import { pluck } from "rxjs/operators";
 
-const onMouseMove$ = fromEvent(document, "mousemove");
-const onMouseDown$ = fromEvent(document, "mousedown");
-
-// A través de takeUntil() definimos que cada vez que un evento es enviado por onMouseDown$
-// el observable sourceCompleted$ será completado. Y por consiguiente no emitirá más valores (línea 15).
-const sourceCompleted$ = onMouseMove$.pipe(takeUntil(onMouseDown$));
-sourceCompleted$.subscribe(console.log);
+fromEvent(document, "mousemove").pipe(pluck("clientX")).subscribe(console.log);
